b1c7c852728184337924674f7d7c6f1840c53f01
Jared-Jacobs <jjacobs3@bellarmine.edu>
2020-01-30 14:56:42 -0500

    final commit

--- SphereProgram/Program.cs
+++ SphereProgram/Program.cs
@@ -9,10 +9,10 @@ namespace SphereProgram
             Sphere sphere = new Sphere();
             sphere.setDiameter(6.5);
 
-            Console.WriteLine("The spheres diameter is " + sphere.getDiameter() + " inches");
-            Console.WriteLine("The surface area of this sphere is " + Math.Round(sphere.getSurfaceArea(), 2) + " square inches");
-            Console.WriteLine("The volume of the sphere is " + Math.Round(sphere.getVolume(), 2) + " cubic inches");
-            Console.WriteLine("The shapes information: " + sphere.ToString());
+            Console.WriteLine("The spheres diameter is: " + sphere.getDiameter() + " inches");
+            Console.WriteLine("The surface area is: " + Math.Round(sphere.getSurfaceArea(), 2) + " square inches");
+            Console.WriteLine("The volume is: " + Math.Round(sphere.getVolume(), 2) + " cubic inches");
+            Console.WriteLine("The spheres information: " + sphere.ToString());
         }
     }
 }
